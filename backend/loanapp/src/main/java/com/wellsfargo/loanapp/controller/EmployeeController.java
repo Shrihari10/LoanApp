a7c047a737de045771f33ed4719d08d0cf6efc54
Shrihari10 <shrihari101307@gmail.com>
2023-08-17 12:54:55 +0530

    feat:backend add route for fetch employee view loan

--- backend/loanapp/src/main/java/com/wellsfargo/loanapp/controller/EmployeeController.java
+++ backend/loanapp/src/main/java/com/wellsfargo/loanapp/controller/EmployeeController.java
@@ -1,7 +1,13 @@
 package com.wellsfargo.loanapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +17,12 @@ import com.wellsfargo.loanapp.model.LoginModel;
 import com.wellsfargo.loanapp.model.EmployeeMaster;
 import com.wellsfargo.loanapp.service.EmployeeService;
 
+import jakarta.validation.Valid;
+
+
+
 @RestController
+@Validated
 @CrossOrigin("http://localhost:3000")
 @RequestMapping("/employee/")
 public class EmployeeController {
@@ -20,15 +31,29 @@ public class EmployeeController {
 	EmployeeService employeeService;
 	
 	@PostMapping("/add")
-	public String saveEmployee(@RequestBody EmployeeMaster employee)
+	//public String saveEmployee(@RequestBody EmployeeMaster employee)
+	public ResponseEntity<EmployeeMaster> saveEmployee(@Valid @RequestBody EmployeeMaster employee, BindingResult bindingResult)
 	{
-	
+		if(bindingResult.hasErrors()) {
+			return new ResponseEntity<> (employee, HttpStatus.BAD_REQUEST);
+		}
+		else {
 		EmployeeMaster createdEmployee = employeeService.saveEmployee(employee);
-		return "Created user with employeeID: " + createdEmployee.getEmployeeID();
+		return new ResponseEntity<> (createdEmployee, HttpStatus.OK);
+		}
+
+//		return "Created user with employeeID: " + createdEmployee.getEmployeeID();
 	}
 	
 	@PostMapping("/login")
 	public String login(@RequestBody LoginModel loginModel) {
 		return employeeService.employeeLogin(loginModel);
 	}
+	
+	@GetMapping("/{employeeId}")
+	public EmployeeMaster getEmployeeDetails(@RequestBody @PathVariable("employeeId") String employeeId ) {
+		return employeeService.getEmployeeDetails(employeeId);
+		
+	}
+	
 }
