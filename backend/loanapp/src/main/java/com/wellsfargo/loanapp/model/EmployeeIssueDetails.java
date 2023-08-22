bee084a199286dbe52d12d453f1056aa49c9f772
atul-yadav-mnit <atulyadav9416@gmail.com>
2023-08-22 17:42:47 +0530

    Added lombok annotations

--- backend/loanapp/src/main/java/com/wellsfargo/loanapp/model/EmployeeIssueDetails.java
+++ backend/loanapp/src/main/java/com/wellsfargo/loanapp/model/EmployeeIssueDetails.java
@@ -12,10 +12,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class EmployeeIssueDetails{
 	
 	@Id
@@ -47,61 +52,4 @@ public class EmployeeIssueDetails{
 	
 	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
 	private Date returnDate;
-
-	public String getIssueId() {
-		return issueId;
-	}
-
-	public void setIssueId(String issueId) {
-		this.issueId = issueId;
-	}
-
-	public EmployeeMaster getEmployee() {
-		return employee;
-	}
-
-	public void setEmployee(EmployeeMaster employee) {
-		this.employee = employee;
-	}
-
-	public ItemMaster getItem() {
-		return item;
-	}
-
-	public void setItem(ItemMaster item) {
-		this.item = item;
-	}
-
-	public Date getIssueDate() {
-		return issueDate;
-	}
-
-	public void setIssueDate(Date issueDate) {
-		this.issueDate = issueDate;
-	}
-
-	public Date getReturnDate() {
-		return returnDate;
-	}
-
-	public void setReturnDate(Date returnDate) {
-		this.returnDate = returnDate;
-	}
-
-	public EmployeeIssueDetails(String issueId, EmployeeMaster employee, ItemMaster item, Date issueDate,
-			Date returnDate) {
-		super();
-		this.issueId = issueId;
-		this.employee = employee;
-		this.item = item;
-		this.issueDate = issueDate;
-		this.returnDate = returnDate;
-	}
-
-	public EmployeeIssueDetails() {
-		super();
-	}
-	
-	
-	
 }
--- backend/loanapp/src/main/java/com/wellsfargo/loanapp/model/EmployeeMaster.java
+++ backend/loanapp/src/main/java/com/wellsfargo/loanapp/model/EmployeeMaster.java
@@ -7,12 +7,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
+@Builder
 public class EmployeeMaster {
 	
 
@@ -43,91 +47,4 @@ public class EmployeeMaster {
 	
 	@Column()
 	private String password;
-
-	public String getEmployeeID() {
-		return employeeID;
-	}
-
-	public void setEmployeeID(String employeeID) {
-		this.employeeID = employeeID;
-	}
-
-	public String getEmployeeName() {
-		return employeeName;
-	}
-
-	public void setEmployeeName(String employeeName) {
-		this.employeeName = employeeName;
-	}
-
-	public String getDesignation() {
-		return designation;
-	}
-
-	public void setDesignation(String designation) {
-		this.designation = designation;
-	}
-
-	public String getDepartment() {
-		return department;
-	}
-
-	public void setDepartment(String department) {
-		this.department = department;
-	}
-
-	public Character getGender() {
-		return gender;
-	}
-
-	public void setGender(Character gender) {
-		this.gender = gender;
-	}
-
-	public LocalDate getDateOfBirth() {
-		return dateOfBirth;
-	}
-
-	public void setDateOfBirth(LocalDate dateOfBirth) {
-		this.dateOfBirth = dateOfBirth;
-	}
-
-	public LocalDate getDateOfJoining() {
-		return dateOfJoining;
-	}
-
-	public void setDateOfJoining(LocalDate dateOfJoining) {
-		this.dateOfJoining = dateOfJoining;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public EmployeeMaster() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public EmployeeMaster(String employeeId, String employeeName, String designation, String department,
-			Character gender, LocalDate dateOfBirth, LocalDate dateOfJoining, String password) {
-		super();
-		this.employeeID = employeeId;
-		this.employeeName = employeeName;
-		this.designation = designation;
-		this.department = department;
-		this.gender = gender;
-		this.dateOfBirth = dateOfBirth;
-		this.dateOfJoining = dateOfJoining;
-		this.password = password;
-	}
-	
-	
-	
-	
-
 }
