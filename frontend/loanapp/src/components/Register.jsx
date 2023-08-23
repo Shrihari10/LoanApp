d150dfd8232bd988b452a1b28e2fbba9e8d9fea8
Shrihari10 <shrihari101307@gmail.com>
2023-08-24 00:07:19 +0530

    feat(frontend): add UI validators for login, register, applyloan

--- frontend/loanapp/src/components/Register.jsx
+++ frontend/loanapp/src/components/Register.jsx
@@ -19,8 +19,75 @@ const Register = ({ user, loginUser }) => {
     }
   }, [user]);
 
+  const [validated, setValidated] = useState(false);
+
+  const [errors, setErrors] = useState({
+    name: "",
+    designation: "",
+    department: "",
+    gender: "",
+    dateOfBirth: "",
+    dateOfJoining: "",
+    password: "",
+  });
+
+  const validateFields = () => {
+    let newErrors = {};
+
+    if (!name) {
+      newErrors.name = "Employee Name is required";
+    }
+
+    if (!department) {
+      newErrors.department = "Department is required";
+    }
+
+    if (!designation) {
+      newErrors.designation = "Designation is required";
+    }
+
+    if (!gender) {
+      newErrors.gender = "Gender is required";
+    } else if (gender !== "M" && gender !== "F") {
+      newErrors.gender = "Gender must be 'M' or 'F'";
+    }
+
+    if (!dob) {
+      newErrors.dateOfBirth = "Date of Birth is required";
+    } else {
+      const currentDate = new Date();
+      const dobTemp = new Date(dob);
+      if (dobTemp >= currentDate) {
+        newErrors.dateOfBirth = "Date of Birth must be before the current date";
+      }
+    }
+
+    if (!doj) {
+      newErrors.dateOfJoining = "Date of Joining is required";
+    } else {
+      const currentDate = new Date();
+      const dojTemp = new Date(doj);
+      if (dojTemp >= currentDate) {
+        newErrors.dateOfJoining =
+          "Date of Joining must be before the current date";
+      }
+    }
+
+    if (!password) {
+      newErrors.password = "Password is required";
+    }
+
+    setErrors(newErrors);
+    return Object.values(newErrors).every((error) => error === "");
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    const isValid = validateFields();
+    if (!isValid) {
+      return;
+    }
+
     const requestBody = {
       employeeName: name,
       designation,
@@ -45,125 +112,167 @@ const Register = ({ user, loginUser }) => {
   };
 
   return (
-    <Container className="d-flex justify-content-center align-items-center h-100">
-      <Form
-        onSubmit={handleSubmit}
-        className="bg-light align-items-center form-inline"
-        style={{ width: "50%", borderRadius: "10px" }}
-      >
-        <h3
-          className="text-warning bg-danger text-center  mb-3 py-2 "
-          style={{
-            width: "100%",
-            borderRadius: "10px 10px 0px 0px",
-            overflow: "hidden",
-          }}
+    <>
+      <Container className="d-flex justify-content-center align-items-center h-100">
+        <Form
+          noValidate
+          validated={validated}
+          onSubmit={handleSubmit}
+          className="bg-light align-items-center form-inline"
+          style={{ width: "50%", borderRadius: "10px" }}
         >
-          Register
-        </h3>
-        <div className="p-4 text-left">
-          <Row>
-            <Col>
-              <Form.Group controlId="name" className="col">
-                <Form.Label>Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                />
-              </Form.Group>
-            </Col>
-            <Col>
-              <Form.Group controlId="designation" className="col">
-                <Form.Label>Designation</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Designation"
-                  value={designation}
-                  onChange={(e) => setDesignation(e.target.value)}
-                />
-              </Form.Group>
-            </Col>
-          </Row>
+          <h3
+            className="text-warning bg-danger text-center  mb-3 py-2 "
+            style={{
+              width: "100%",
+              borderRadius: "10px 10px 0px 0px",
+              overflow: "hidden",
+            }}
+          >
+            Register
+          </h3>
+          <div className="p-4 text-left">
+            <Row>
+              <Col>
+                <Form.Group controlId="name" className="col">
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control
+                    required
+                    type="text"
+                    placeholder="Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    isInvalid={errors.name}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.name}
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="designation" className="col">
+                  <Form.Label>Designation</Form.Label>
+                  <Form.Control
+                    required
+                    type="text"
+                    placeholder="Designation"
+                    value={designation}
+                    onChange={(e) => setDesignation(e.target.value)}
+                    isInvalid={errors.designation}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.designation}
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </Col>
+            </Row>
 
-          <Form.Group controlId="department">
-            <Form.Label>Department</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Department"
-              value={department}
-              onChange={(e) => setDepartment(e.target.value)}
-            />
-          </Form.Group>
+            <Form.Group controlId="department">
+              <Form.Label>Department</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Department"
+                value={department}
+                onChange={(e) => setDepartment(e.target.value)}
+                isInvalid={errors.department}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.department}
+              </Form.Control.Feedback>
+            </Form.Group>
 
-          <Form.Group controlId="gender">
-            <Form.Label>Gender</Form.Label>
-            <div>
-              <Form.Check
-                type="radio"
-                label="Male"
-                name="gender"
-                value="M"
-                checked={gender === "M"}
-                onChange={(e) => setGender(e.target.value)}
-                inline
-              />
-              <Form.Check
-                type="radio"
-                label="Female"
-                name="gender"
-                value="F"
-                checked={gender === "F"}
-                onChange={(e) => setGender(e.target.value)}
-                inline
-              />
-            </div>
-          </Form.Group>
+            <Form.Group controlId="gender">
+              <Form.Label>Gender</Form.Label>
+              <div>
+                <Form.Check
+                  type="radio"
+                  label="Male"
+                  name="gender"
+                  value="M"
+                  checked={gender === "M"}
+                  onChange={(e) => setGender(e.target.value)}
+                  inline
+                  isInvalid={errors.gender}
+                />
+                <Form.Check
+                  type="radio"
+                  label="Female"
+                  name="gender"
+                  value="F"
+                  checked={gender === "F"}
+                  onChange={(e) => setGender(e.target.value)}
+                  inline
+                  isInvalid={errors.gender}
+                />
+                <Form.Control.Feedback
+                  type="invalid"
+                  style={{ display: "block" }}
+                >
+                  {errors.gender}
+                </Form.Control.Feedback>
+              </div>
+            </Form.Group>
 
             <Row>
               <Col>
-          <Form.Group controlId="dob">
-            <Form.Label>Date of Birth</Form.Label>
-            <Form.Control
-              type="date"
-              value={dob}
-              onChange={(e) => setDob(e.target.value)}
-            />
-          </Form.Group>
-          </Col>
-          <Col>
-          <Form.Group controlId="doj">
-            <Form.Label>Date of Joining</Form.Label>
-            <Form.Control
-              type="date"
-              value={doj}
-              onChange={(e) => setDoj(e.target.value)}
-            />
-          </Form.Group>
-            </Col>
+                <Form.Group controlId="dob">
+                  <Form.Label>Date of Birth</Form.Label>
+                  <Form.Control
+                    required
+                    type="date"
+                    value={dob}
+                    onChange={(e) => setDob(e.target.value)}
+                    isInvalid={errors.dateOfBirth}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.dateOfBirth}
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="doj">
+                  <Form.Label>Date of Joining</Form.Label>
+                  <Form.Control
+                    required
+                    type="date"
+                    value={doj}
+                    onChange={(e) => setDoj(e.target.value)}
+                    isInvalid={errors.dateOfBirth}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.dateOfJoining}
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </Col>
             </Row>
-          <Form.Group controlId="password">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Group>
-        </div>
+            <Form.Group controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                required
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                isInvalid={errors.password}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.password}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </div>
 
-        <div className="text-center">
-          <Button variant="primary" type="submit">
-            Sign Up
-          </Button>
-          <p className="forgot-password mt-3">
-            Already registered? <a href="/login">Login</a>
-          </p>
-        </div>
-      </Form>
-    </Container>
+          <div className="text-center">
+            <Button variant="primary" type="submit">
+              Sign Up
+            </Button>
+            <p className="forgot-password mt-3">
+              Already registered? <a href="/login">Login</a>
+            </p>
+          </div>
+        </Form>
+      </Container>
+    </>
   );
 };
 
