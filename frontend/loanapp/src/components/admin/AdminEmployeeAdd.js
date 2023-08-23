1ac6f74c5cd9ca67632305335a9b906ac4e33fa4
lokeshtejavath <lokinaikkumar.t@gmail.com>
2023-08-23 19:00:22 +0530

    feat(frontend): validation in employee add page

--- frontend/loanapp/src/components/admin/AdminEmployeeAdd.js
+++ frontend/loanapp/src/components/admin/AdminEmployeeAdd.js
@@ -3,38 +3,277 @@ import { Form, Button, Container } from 'react-bootstrap';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const AdminEmployeeAdd = ({user}) => {
-  const [name, setName] = useState('');
-  const [designation, setDesignation] = useState('');
-  const [department, setDepartment] = useState('');
-  const [gender, setGender] = useState('');
-  const [dob, setDob] = useState('');
-  const [doj, setDoj] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useNavigate();
+// const AdminEmployeeAdd = ({user}) => {
+//   const [name, setName] = useState('');
+//   const [designation, setDesignation] = useState('');
+//   const [department, setDepartment] = useState('');
+//   const [gender, setGender] = useState('');
+//   const [dob, setDob] = useState('');
+//   const [doj, setDoj] = useState('');
+//   const [password, setPassword] = useState('');
+//   const navigate = useNavigate();
 
+//   const [errors, setErrors] = useState({
+//     name: '',
+//     designation: '',
+//     department: '',
+//     gender: '',
+//     dob: '',
+//     doj: '',
+//     password: ''
+//   });
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     const requestBody = {
+//       employeeName: name,
+//       designation,
+//       department,
+//       gender,
+//       dateOfBirth: dob,
+//       dateOfJoining: doj,
+//       password
+//     };
+
+//     const validateFields = () => {
+//       let newErrors = {};
+//       let formIsValid = true;
+
+//       if(!name || name === '') {
+//         formIsValid = false;
+//         newErrors['name'] = 'Name cannot be empty';
+//       }
+//       if (!designation || designation === '') {
+//         formIsValid = false;
+//         newErrors['designation'] = 'Designation cannot be empty';
+//       }
+//       if (!department || department === '') {
+//         formIsValid = false;
+//         newErrors['department'] = 'Department cannot be empty';
+//       }
+//       if(!gender|| gender === '') {
+//         formIsValid = false;
+//         newErrors['gender']='Please select a gender'
+//       }
+//       //check if dob is not empty and is a valid date and is not a future date
+//       if(!dob || dob === '' || new Date(dob) > new Date()) {
+//         formIsValid = false;
+//         newErrors['dob']='Please enter a valid date of birth'
+//       }
+//       //check if doj is not empty and is a valid date and is not a future date
+//       if(!doj || doj === '' || new Date(doj) > new Date()) {
+//         formIsValid = false;
+//         newErrors['doj']='Please enter a valid date of joining'
+//       }
+//       if(!password || password === '' || password.length < 8) {
+//         formIsValid = false;
+//         newErrors['password']='Password should be atleast 8 characters long'
+//       }
+//       setErrors(newErrors);
+//       return formIsValid;
+//     }
+
+//     if(!validateFields()) {
+//       return;
+//     }
+
+//     axios.post("http://localhost:8080/employee/add", requestBody)
+//       .then((res) => {
+//         alert("Employee created with Id: " + res.data.body.employeeID);
+//         navigate("/admin/employee/edit");
+//         console.log(JSON.stringify(res.data)); 
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//         alert("Error: " + err);
+//       });
+//   };
+
+//   return (
+//     <Container className="d-flex justify-content-center align-items-center ">
+//       <Form onSubmit={handleSubmit} className="p-3 bg-light align-items-center form-inline" style={{ width: '50%',marginTop:'30px',borderRadius:'10px' }}>
+//         <h3 className="text-warning bg-danger text-center mb-3">Add New Employee</h3>
+//         <Form.Group controlId="name">
+//           <Form.Label>Name</Form.Label>
+//           <Form.Control
+//             type="text"
+//             placeholder="Name"
+//             value={name}
+//             onChange={(e) => setName(e.target.value)}
+//             isInvalid={errors.name}
+//           />
+//           <Form.Control.Feedback type="invalid">
+//             {errors.name}
+//           </Form.Control.Feedback>
+//         </Form.Group>
+
+//         <Form.Group controlId="designation">
+//           <Form.Label>Designation</Form.Label>
+//           <Form.Control
+//             type="text"
+//             placeholder="Designation"
+//             value={designation}
+//             onChange={(e) => setDesignation(e.target.value)}
+//             isInvalid={errors.designation}
+//           />
+//           <Form.Control.Feedback type="invalid">
+//             {errors.designation}
+//           </Form.Control.Feedback>
+//         </Form.Group>
+
+//         <Form.Group controlId="department">
+//           <Form.Label>Department</Form.Label>
+//           <Form.Control
+//             type="text"
+//             placeholder="Department"
+//             value={department}
+//             onChange={(e) => setDepartment(e.target.value)}
+//           />
+//           <Form.Control.Feedback type="invalid">
+//             {errors.department}
+//           </Form.Control.Feedback>
+//         </Form.Group>
+
+//         <Form.Group controlId="gender">
+//           <Form.Label>Gender</Form.Label>
+//           <div>
+//             <Form.Check
+//               type="radio"
+//               label="Male"
+//               name="gender"
+//               value="M"
+//               checked={gender === 'M'}
+//               onChange={(e) => setGender(e.target.value)}
+//               inline
+//             />
+//             <Form.Check
+//               type="radio"
+//               label="Female"
+//               name="gender"
+//               value="F"
+//               checked={gender === 'F'}
+//               onChange={(e) => setGender(e.target.value)}
+//               inline
+//             />
+//           </div>
+//         </Form.Group>
+
+//         <Form.Group controlId="dob">
+//           <Form.Label>Date of Birth</Form.Label>
+//           <Form.Control
+//             type="date"
+//             value={dob}
+//             onChange={(e) => setDob(e.target.value)}
+//             isInvalid={errors.dob}
+//           />
+//           <Form.Control.Feedback type="invalid">
+//             {errors.dob}
+//           </Form.Control.Feedback>
+
+//         </Form.Group>
+
+//         <Form.Group controlId="doj">
+//           <Form.Label>Date of Joining</Form.Label>
+//           <Form.Control
+//             type="date"
+//             value={doj}
+//             onChange={(e) => setDoj(e.target.value)}
+//           />
+//           <Form.Control.Feedback type="invalid">
+//             {errors.doj}
+//           </Form.Control.Feedback>
+//         </Form.Group>
+//         <Form.Group controlId="password">
+//           <Form.Label>Password</Form.Label>
+//           <Form.Control
+//             type="password"
+//             placeholder="Password"
+//             value={password}
+//             onChange={(e) => setPassword(e.target.value)} 
+//             isInvalid={errors.password} 
+//           />
+//           <Form.Control.Feedback type="invalid">
+//             {errors.password}
+//           </Form.Control.Feedback>
+//         </Form.Group>
+
+//         <div className="text-center">
+//           <Button variant="primary" type="submit">
+//             Add
+//           </Button>
+//           {/* <p className="forgot-password mt-3">
+//             Already registered? <a href="/login">Login</a>
+//           </p> */}
+//         </div>
+//       </Form>
+//     </Container>
+//   );
+// };
+
+
+
+const AdminEmployeeAdd = ({user}) => {
+  const [userDetails, setUserDetails] = useState([]);
+  const userName = sessionStorage.getItem('userName');
+  const [errors, setErrors] = useState({
+    name: '',
+    designation: '',
+    department: '',
+    gender: '',
+    dob: '',
+    doj: '',
+    password: ''
+  });
+  const validateFields = () => {
+    let newErrors = {};
+    if(!userDetails.employeeName || userDetails.employeeName === '') {
+      newErrors['name'] = 'Name cannot be empty';
+    }
+    if (!userDetails.designation || userDetails.designation === '') {
+      newErrors['designation'] = 'Designation cannot be empty';
+    }
+    if (!userDetails.department || userDetails.department === '') {
+      newErrors['department'] = 'Department cannot be empty';
+    }
+    if(!userDetails.dob || userDetails.dob === '' || new Date(userDetails.dob) > new Date()) {
+      newErrors['dob']='Please enter a valid date of birth'
+    }
+    if(!userDetails.doj || userDetails.doj === '' || new Date(userDetails.doj) > new Date()) {
+      newErrors['doj']='Please enter a valid date of joining'
+    }
+    if(!userDetails.password || userDetails.password === '' || userDetails.password.length < 8) {
+      newErrors['password']='Password should be atleast 8 characters long'
+    }
+    setErrors(newErrors);
+    return Object.keys(newErrors).every(x => newErrors[x] === '');
+  }
+
+ const handleChanges = (e) => {
+    setUserDetails({
+      ...userDetails,
+      [e.target.name]: e.target.value
+    });
+  }
+
+  const navigate = useNavigate();
   const handleSubmit = (e) => {
     e.preventDefault();
-    const requestBody = {
-      employeeName: name,
-      designation,
-      department,
-      gender,
-      dateOfBirth: dob,
-      dateOfJoining: doj,
-      password
-    };
-
-    axios.post("http://localhost:8080/employee/add", requestBody)
+    const errorsVal = validateFields();
+    console.log(errorsVal);
+    if(!errorsVal) {
+      return;
+    }
+    axios.post("http://localhost:8080/employee/add", userDetails)
       .then((res) => {
         alert("Employee created with Id: " + res.data.body.employeeID);
         navigate("/admin/employee/edit");
-        console.log(JSON.stringify(res.data)); 
+        console.log(JSON.stringify(res.data));
       })
       .catch((err) => {
         console.log(err);
         alert("Error: " + err);
       });
+
   };
 
   return (
@@ -46,89 +285,105 @@ const AdminEmployeeAdd = ({user}) => {
           <Form.Control
             type="text"
             placeholder="Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            value={userDetails.employeeName}
+            onChange={handleChanges}
+            isInvalid={errors.name}
           />
+          <Form.Control.Feedback type="invalid">
+            {errors.name}
+          </Form.Control.Feedback>
         </Form.Group>
-
         <Form.Group controlId="designation">
           <Form.Label>Designation</Form.Label>
           <Form.Control
             type="text"
             placeholder="Designation"
-            value={designation}
-            onChange={(e) => setDesignation(e.target.value)}
+            value={userDetails.designation}
+            onChange={handleChanges}
+            isInvalid={errors.designation}
           />
+          <Form.Control.Feedback type="invalid">
+            {errors.designation}  
+          </Form.Control.Feedback>
         </Form.Group>
-
         <Form.Group controlId="department">
           <Form.Label>Department</Form.Label>
           <Form.Control
             type="text"
             placeholder="Department"
-            value={department}
-            onChange={(e) => setDepartment(e.target.value)}
+            value={userDetails.department}
+            onChange={handleChanges}
           />
+          <Form.Control.Feedback type="invalid">
+            {errors.department}
+          </Form.Control.Feedback>
         </Form.Group>
-
         <Form.Group controlId="gender">
           <Form.Label>Gender</Form.Label>
           <div>
             <Form.Check
-              type="radio"
+              type="radio"  
               label="Male"
               name="gender"
               value="M"
-              checked={gender === 'M'}
-              onChange={(e) => setGender(e.target.value)}
+              checked={userDetails.gender==='M'}
+              onChange={handleChanges}
               inline
             />
             <Form.Check
-              type="radio"
+              type="radio"  
               label="Female"
               name="gender"
               value="F"
-              checked={gender === 'F'}
-              onChange={(e) => setGender(e.target.value)}
+              checked={userDetails.gender==='F'}
+              onChange={handleChanges}
               inline
             />
           </div>
-        </Form.Group>
-
-        <Form.Group controlId="dob">
+          </Form.Group>
+          <Form.Group controlId="dob">
           <Form.Label>Date of Birth</Form.Label>
           <Form.Control
             type="date"
-            value={dob}
-            onChange={(e) => setDob(e.target.value)}
+            value={userDetails.dob}
+            onChange={handleChanges}
+            isInvalid={errors.dob}
           />
-        </Form.Group>
-
-        <Form.Group controlId="doj">
+          <Form.Control.Feedback type="invalid">
+            {errors.dob}
+          </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group controlId="doj">
           <Form.Label>Date of Joining</Form.Label>
           <Form.Control
+
             type="date"
-            value={doj}
-            onChange={(e) => setDoj(e.target.value)}
+            value={userDetails.doj}
+            onChange={handleChanges}
+            isInvalid={errors.doj}
           />
-        </Form.Group>
-        <Form.Group controlId="password">
+          <Form.Control.Feedback type="invalid">
+            {errors.doj}
+          </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group controlId="password">
           <Form.Label>Password</Form.Label>
           <Form.Control
-            type="password"
+            type="password" 
             placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}  
+            value={userDetails.password}
+            onChange={handleChanges}
+            isInvalid={errors.password}
           />
-        </Form.Group>
-
-        <div className="text-center">
-          <Button variant="primary" type="submit">
+          <Form.Control.Feedback type="invalid">
+            {errors.password}
+          </Form.Control.Feedback>
+          </Form.Group>
+          <div className="text-center">
+          <Button variant="primary" type="submit">  
             Add
           </Button>
-          {/* <p className="forgot-password mt-3">
-            Already registered? <a href="/login">Login</a>
-          </p> */}
+          
         </div>
       </Form>
     </Container>
