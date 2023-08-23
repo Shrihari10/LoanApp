af3dabc0da58fd2eec92d29e131d32ff94ee8309
Shrihari10 <62280755+Shrihari10@users.noreply.github.com>
2023-08-24 00:09:06 +0530

    feat(frontend): add UI validators for login, register, applyloan (#61)

--- frontend/loanapp/src/components/ApplyLoan.js
+++ frontend/loanapp/src/components/ApplyLoan.js
@@ -1,171 +1,226 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { Button, Form, Container } from 'react-bootstrap';
-import { useNavigate } from 'react-router';
-import { Navigate } from 'react-router-dom';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Button, Form, Container } from "react-bootstrap";
+import { useNavigate } from "react-router";
+import { Navigate } from "react-router-dom";
 
 function ApplyLoan() {
-let [loanCards, setLoanCards ] = useState([]);
+  let [loanCards, setLoanCards] = useState([]);
 
-    let [empId, setEmpId] = useState(sessionStorage.getItem("username"));
-    let [items, setItems] = useState([]);
-    let [itemCategory, setItemCategory] = useState([]);
-    let [selectedItemCategory, setSelectedItemCategory] = useState('');
-    let [itemDescription, setItemDescription] = useState([]);
-    let [selectedItemDescription, setSelectedItemDescription] = useState('');
-    let [itemValue, setItemValue] = useState('');
-    let [itemMake, setItemMake] = useState([]);
-    let [selectedItemMake, setSelectedItemMake] = useState('');
-    const navigate = useNavigate(); 
+  let [empId, setEmpId] = useState(sessionStorage.getItem("username"));
+  let [items, setItems] = useState([]);
+  let [itemCategory, setItemCategory] = useState([]);
+  let [selectedItemCategory, setSelectedItemCategory] = useState("");
+  let [itemDescription, setItemDescription] = useState([]);
+  let [selectedItemDescription, setSelectedItemDescription] = useState("");
+  let [itemValue, setItemValue] = useState("");
+  let [itemMake, setItemMake] = useState([]);
+  let [selectedItemMake, setSelectedItemMake] = useState("");
+  const navigate = useNavigate();
 
-    useEffect(()=>{
-        axios.get("http://localhost:8080/loanCard/all")
+  const [validated, setValidated] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/loanCard/all")
       .then((res) => {
-        console.log(res.data.message);
         setLoanCards(res.data.body);
-        setItemCategory(res.data.body.map(obj => obj["loanType"]));
+        setItemCategory(res.data.body.map((obj) => obj["loanType"]));
       })
       .catch((err) => {
         console.log(err);
         alert("Error: " + err);
       });
-    }, []);
+  }, []);
 
-    useEffect(()=>{
-        axios.get("http://localhost:8080/item/all")
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/item/all")
       .then((res) => {
         setItems(res.data.body);
-        console.log(items);
       })
       .catch((err) => {
         console.log(err);
         alert("Error: " + err);
       });
-    }, []);
+  }, []);
 
-    // useEffect(() => {
-    //   // const categories = getUniqueValuesByKey(items, "itemCategory");
-    //   console.log(items);
-    //   let categories = items.map(obj => obj['itemCategory']);
-    //   categories = [...new Set(categories)];
-    //   console.log(categories);
-    //   setItemCategory(categories);
-    // }, [items])
+  useEffect(() => {
+    const filteredItemMakes = items
+      .filter((item) => item.itemCategory == selectedItemCategory)
+      .map((obj) => obj["itemMake"]);
+    setItemMake([...new Set(filteredItemMakes)]);
+    setSelectedItemMake("");
+  }, [selectedItemCategory, items]);
 
-    useEffect(() => {
-      const filteredItemMakes = items.filter((item) => item.itemCategory == selectedItemCategory).map(obj => obj['itemMake']);
-      console.log(filteredItemMakes);
-      setItemMake([...new Set(filteredItemMakes)]);
-      setSelectedItemMake('');
-    }, [selectedItemCategory, items])
+  useEffect(() => {
+    const filteredItemDescription = items
+      .filter(
+        (item) =>
+          item.itemCategory == selectedItemCategory &&
+          item.itemMake == selectedItemMake
+      )
+      .map((obj) => obj["itemDescription"]);
+    setItemDescription([...new Set(filteredItemDescription)]);
+    setSelectedItemDescription("");
+  }, [selectedItemMake]);
 
-    useEffect(() => {
-      const filteredItemDescription = items.filter((item) => item.itemCategory == selectedItemCategory && item.itemMake == selectedItemMake).map(obj => obj['itemDescription']);
-      console.log(filteredItemDescription);
-      setItemDescription([...new Set(filteredItemDescription)]);
-      setSelectedItemDescription('');
-    }, [selectedItemMake])
+  useEffect(() => {
+    const filteredItemValue = items
+      .filter(
+        (item) =>
+          item.itemCategory == selectedItemCategory &&
+          item.itemMake == selectedItemMake &&
+          item.itemDescription == selectedItemDescription
+      )
+      .map((obj) => obj["itemValuation"]);
+    setItemValue(filteredItemValue.length > 0 ? filteredItemValue[0] : "");
+  }, [selectedItemDescription]);
 
-    useEffect(() => {
-      const filteredItemValue = items.filter((item) => item.itemCategory == selectedItemCategory && item.itemMake == selectedItemMake && item.itemDescription == selectedItemDescription).map(obj => obj['itemValuation']);
-      setItemValue(filteredItemValue.length > 0 ? filteredItemValue[0] : '');
-    }, [selectedItemDescription])
+  const [errors, setErrors] = useState({
+    itemCategory: "",
+    itemMake: "",
+    itemDescription: "",
+  });
 
+  const validateFields = () => {
+    let newErrors = {};
 
-    const handleSubmit = (e) =>{
-            e.preventDefault();
-        let filteredLoanCards = loanCards.filter((obj) => (obj["loanType"] === selectedItemCategory));
-        if (filteredLoanCards.length == 0) {
-            alert('please choose category');
-            return;
-        }
-        const loanCardId = filteredLoanCards[0].loanId; 
-
-        let filteredItems = items.filter((obj) => (obj["itemCategory"] === selectedItemCategory && obj["itemMake"] === selectedItemMake && obj["itemDescription"] === selectedItemDescription));
-        if (filteredItems.length == 0) {
-            alert('please choose all fields');
-            return;
-        }
-        let itemId = filteredItems[0].itemId;
-
-        const requestBody = {
-            employeeId: empId,
-            loanCardId: loanCardId,
-            itemId: itemId
-        };
-
-        axios.post("http://localhost:8080/loan/apply", requestBody)
-        .then((res) => {
-            alert(res.data.message);
-            navigate(`/loan/all`)
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+    if (!selectedItemCategory) {
+      newErrors.itemCategory = "Please select Item Category!";
+    }
+    if (!selectedItemMake) {
+      newErrors.itemMake = "Please select Item Make";
+    }
+    if (!selectedItemDescription) {
+      newErrors.itemDescription = "Please select Item Description";
     }
 
+    setErrors(newErrors);
+    return Object.values(newErrors).every((error) => error === "");
+  };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const isValid = validateFields();
+    if (!isValid) {
+      return;
+    }
+
+    let filteredLoanCards = loanCards.filter(
+      (obj) => obj["loanType"] === selectedItemCategory
+    );
+    if (filteredLoanCards.length == 0) {
+      alert("please choose category");
+      return;
+    }
+    const loanCardId = filteredLoanCards[0].loanId;
+
+    let filteredItems = items.filter(
+      (obj) =>
+        obj["itemCategory"] === selectedItemCategory &&
+        obj["itemMake"] === selectedItemMake &&
+        obj["itemDescription"] === selectedItemDescription
+    );
+    if (filteredItems.length == 0) {
+      alert("please choose all fields");
+      return;
+    }
+    let itemId = filteredItems[0].itemId;
+
+    const requestBody = {
+      employeeId: empId,
+      loanCardId: loanCardId,
+      itemId: itemId,
+    };
+
+    axios
+      .post("http://localhost:8080/loan/apply", requestBody)
+      .then((res) => {
+        alert(res.data.message);
+        navigate(`/loan/all`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <Container className="d-flex justify-content-center align-items-center min-vh-100">
-      <Form onSubmit={handleSubmit} className="p-3 bg-light align-items-center" style={{ width: '50%' }}>
-        <h3 className="text-warning bg-danger text-center mb-3">Select Product and Apply for Loan</h3>
-        
-        
+      <Form
+        noValidate
+        validated={validated}
+        onSubmit={handleSubmit}
+        className="p-3 bg-light align-items-center"
+        style={{ width: "50%" }}
+      >
+        <h3 className="text-warning bg-danger text-center mb-3">
+          Select Product and Apply for Loan
+        </h3>
+
         <Form.Group controlId="empId">
           <Form.Label> Employee ID</Form.Label>
-          <Form.Control
-            type="text"
-            value={empId}
-            disabled
-          />
+          <Form.Control type="text" value={empId} disabled />
         </Form.Group>
 
         <Form.Group controlId="Item Category">
           <Form.Label>Item Category</Form.Label>
-          <Form.Select aria-label="Default select example" value={selectedItemCategory} onChange={(event) => setSelectedItemCategory(event.target.value)}>
-      <option>Open this select menu</option>
-      {itemCategory.map((item) => 
-            <option value={item}>{item}</option>
-      )}
-      
-      
-    </Form.Select>
+          <Form.Select
+            aria-label="Default select example"
+            value={selectedItemCategory}
+            onChange={(event) => setSelectedItemCategory(event.target.value)}
+            isInvalid={errors.itemCategory}
+          >
+            <option>Open this select menu</option>
+            {itemCategory.map((item) => (
+              <option value={item}>{item}</option>
+            ))}
+          </Form.Select>
+          <Form.Control.Feedback type="invalid">
+            {errors.itemCategory}
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group controlId="Item Make">
           <Form.Label>Item Make</Form.Label>
-          <Form.Select aria-label="Default select example" onChange={(e) => setSelectedItemMake(e.target.value)}>
-      <option>Open this select menu</option>
-      {itemMake.map(item => (
-            <option value={item}>{item}</option>
-      ))}
-      
-    </Form.Select>
+          <Form.Select
+            aria-label="Default select example"
+            value={selectedItemMake}
+            onChange={(e) => setSelectedItemMake(e.target.value)}
+            isInvalid={errors.itemMake}
+          >
+            <option>Open this select menu</option>
+            {itemMake.map((item) => (
+              <option value={item}>{item}</option>
+            ))}
+          </Form.Select>
+          <Form.Control.Feedback type="invalid">
+            {errors.itemMake}
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group controlId="Item Description">
           <Form.Label>Item Description</Form.Label>
-          <Form.Select aria-label="Default select example" value={selectedItemDescription} onChange={(e) => setSelectedItemDescription(e.target.value)}>
-      <option>Open this select menu</option>
-      {itemDescription.map(item => (
-            <option value={item} >{item}</option>
-      ))}
-
-      
-    </Form.Select>
+          <Form.Select
+            aria-label="Default select example"
+            value={selectedItemDescription}
+            onChange={(e) => setSelectedItemDescription(e.target.value)}
+            isInvalid={errors.itemDescription}
+          >
+            <option>Open this select menu</option>
+            {itemDescription.map((item) => (
+              <option value={item}>{item}</option>
+            ))}
+          </Form.Select>
+          <Form.Control.Feedback type="invalid">
+            {errors.itemDescription}
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group controlId="Item Value">
           <Form.Label>Item Value</Form.Label>
-          <Form.Control
-            type="text"
-            value={itemValue}
-            disabled
-          />
+          <Form.Control type="text" value={itemValue} disabled />
         </Form.Group>
-
-        
 
         <div className="text-center">
           <Button variant="primary" type="submit">
@@ -174,7 +229,7 @@ let [loanCards, setLoanCards ] = useState([]);
         </div>
       </Form>
     </Container>
-    )
-  }
+  );
+}
 
-export default ApplyLoan
+export default ApplyLoan;
--- frontend/loanapp/src/components/Login.jsx
+++ frontend/loanapp/src/components/Login.jsx
@@ -1,109 +1,176 @@
-import React, {useEffect, useState} from "react";
-import {Form, Button, Container} from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Form, Button, Container } from "react-bootstrap";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const Login = ({ user, role, loginUser}) => {
-    const [employeeID, setEmployeeID] = useState("");
-    const [password, setPassword] = useState("")
-    const navigate = useNavigate();
+const Login = ({ user, role, loginUser }) => {
+  const [submitAdmin, setSubmitAdmin] = useState(true);
+  const [employeeID, setEmployeeID] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        // console.log(role, " ", user);
-         if(role ==="admin"){
-            navigate('/');
-        }
-        else if(role ==="user"){
-            navigate('/');
-        }
-    },[user])
+  const [validated, setValidated] = useState(false);
 
-    const handleUserLogin = (e) => {
-        e.preventDefault();
-        const requestBody = {
-            employeeID,
-            password
-        };
-        
-        // loginUser(employeeID);
+  const [errors, setErrors] = useState({
+    employeeID: "",
+    password: "",
+  });
 
-        axios.post("http://localhost:8080/employee/login", requestBody)
-            .then((res) => {
-                alert(res.data.message);
-                if (res.data.message.includes('successfull')) {
-                    loginUser(employeeID, "user");
-                }
-            })
-            .catch((err) => {
-                console.log(err);
-                alert("Error: " + err);
-            });
+  const validateFields = () => {
+    let newErrors = {};
+    if (!employeeID) {
+      newErrors.employeeID = "Please enter Employee ID!";
+    }
+    if (!password) {
+      newErrors.password = "Please enter your Password!";
+    }
+
+    setErrors(newErrors);
+    return Object.values(newErrors).every((error) => error === "");
+  };
+
+  useEffect(() => {
+    if (role === "admin") {
+      navigate("/");
+    } else if (role === "user") {
+      navigate("/");
+    }
+  }, [user]);
+
+  const handleUserLogin = (e) => {
+    e.preventDefault();
+    const isValid = validateFields();
+    if (!isValid) {
+      return;
+    }
+
+    const requestBody = {
+      employeeID,
+      password,
     };
 
-    
-    const handleAdminLogin = (e) => {
-        e.preventDefault();
-        const requestBody = {
-            username: employeeID,
-            password
-        };
-        
-        // loginUser(employeeID);
+    axios
+      .post("http://localhost:8080/employee/login", requestBody)
+      .then((res) => {
+        alert(res.data.message);
+        if (res.data.message.includes("successfull")) {
+          loginUser(employeeID, "user");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Error: " + err);
+      });
+  };
 
-        axios.post("http://localhost:8080/admin/login", requestBody)
-            .then((res) => {
-                alert(res.data.message);
-                if (res.data.message.includes('Successfully')) {
-                    loginUser(employeeID, "admin");
-                }
-            })
-            .catch((err) => {
-                console.log(err);
-                alert("Error: " + err);
-            });
+  const handleAdminLogin = (e) => {
+    e.preventDefault();
+    const isValid = validateFields();
+    if (!isValid) {
+      return;
+    }
+
+    const requestBody = {
+      username: employeeID,
+      password,
     };
 
-    return (
-        <Container className="d-flex justify-content-center align-items-center h-100">
-            <Form  className=" bg-light align-items-center" style={{width: '50%',borderRadius:'10px'}}>
-                <h3 className="text-warning bg-danger text-center  mb-3 py-2 " style={{width: '100%', borderRadius: '10px 10px 0px 0px', overflow:'hidden'}}>Login</h3>
-              <div className="p-4 text-left">
-                <Form.Group controlId="employeeID">
-                    <Form.Label>Employee ID</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Employee ID"
-                        value={employeeID}
-                        onChange={(e) => setEmployeeID(e.target.value)}
-                    />
-                </Form.Group>
-                <Form.Group className="text-left w-90" controlId="password">
-                    <Form.Label className="text-left w-90 ml-2 pl-2">Password</Form.Label>
-                    <Form.Control
-                        type="password"
-                        placeholder="Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <br/>
-                </Form.Group>
-                <div className="text-center">
-                    <Button variant="primary" type="submit" onClick={handleUserLogin}>
-                        Login as User
-                    </Button>
-                    {' '}
-                    <Button variant="danger" type="submit" onClick={handleAdminLogin}>
-                        Login as Admin
-                    </Button>
-                    <p className="forgot-password text-right">
-                        <a href="#">Forgot password?</a>
-                    </p>
-                </div>
+    axios
+      .post("http://localhost:8080/admin/login", requestBody)
+      .then((res) => {
+        alert(res.data.message);
+        if (res.data.message.includes("Successfully")) {
+          loginUser(employeeID, "admin");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Error: " + err);
+      });
+  };
 
-                </div>
-            </Form>
-        </Container>
-    );
+  return (
+    <Container className="d-flex justify-content-center align-items-center text-left h-100">
+      <Form
+        noValidate
+        validated={validated}
+        onSubmit={submitAdmin ? handleAdminLogin : handleUserLogin}
+        className=" bg-light align-items-center"
+        style={{ width: "40%", borderRadius: "10px" }}
+      >
+        <h3
+          className="text-warning bg-danger text-center  mb-3 py-2 "
+          style={{
+            width: "100%",
+            borderRadius: "10px 10px 0px 0px",
+            overflow: "hidden",
+          }}
+        >
+          Login
+        </h3>
+        <div className="p-4 text-left">
+          <Form.Group controlId="employeeID">
+            <Form.Label
+              className="w-100 text-left"
+              style={{ textAlign: "left", paddingLeft: "5px" }}
+            >
+              {" "}
+              Employee ID
+            </Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="Employee ID"
+              value={employeeID}
+              onChange={(e) => setEmployeeID(e.target.value)}
+              isInvalid={errors.employeeID}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.employeeID}
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="text-left w-90" controlId="password">
+            <Form.Label
+              className="text-left mt-3 w-100 ml-2 pl-2"
+              style={{ textAlign: "left", paddingLeft: "5px" }}
+            >
+              Password
+            </Form.Label>
+            <Form.Control
+              required
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              isInvalid={errors.password}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.password}
+            </Form.Control.Feedback>
+            <br />
+          </Form.Group>
+          <div className="text-center">
+            <Button
+              variant="primary"
+              type="submit"
+              onClick={() => setSubmitAdmin(false)}
+            >
+              Login as User
+            </Button>{" "}
+            <Button variant="danger" type="submit">
+              Login as Admin
+            </Button>
+            <p
+              className="forgot-password text-right mt-2 "
+              style={{ color: "blue", textDecoration: "underline" }}
+            >
+              <a href="#">Forgot password?</a>
+            </p>
+          </div>
+        </div>
+      </Form>
+    </Container>
+  );
 };
 
 export default Login;
