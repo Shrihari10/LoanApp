0333e632e8de4663918a964c73ac768a2492956e
lokeshtejavath <lokinaikkumar.t@gmail.com>
2023-08-25 15:01:56 +0530

    feat(frontend): rework view load and view item foe user using chakra-ui

--- frontend/loanapp/src/components/ViewItem.js
+++ frontend/loanapp/src/components/ViewItem.js
@@ -2,7 +2,12 @@ import React, { useEffect, useState} from 'react'
 import axios from "axios";
 import { successToast, failureToast } from "../utils/ToastUtils";
 import { getAllEmployeeIssues, getEmployee } from "../api/service";
+import { Heading, Text } from "@chakra-ui/react";
+import { Card, CardBody, CardFooter, CardHeader } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 
+import { SimpleGrid } from "@chakra-ui/react";
+import { Stack, HStack } from "@chakra-ui/react";
 
 const ViewItem = () => {
   const [issues, setIssues] = useState([]);
@@ -35,45 +40,89 @@ const ViewItem = () => {
   }, []);
 
   return (
-    <>
-      <h1>Purchased Items</h1>
-      <div
-        style={{
-          width: "100%",
-          display: "flex",
-          justifyContent: "space-evenly",
-        }}
-      >
-        <span> Employee ID : {username}</span>
+   <div className="container" >
+      <Card direction={{base:"column",sm:"row"}}
+        variant="outline"
+        position="sticky"
+        top="1"
+        zIndex="1"
+        marginTop="2rem"
+        
+>
+<CardHeader fontSize="md" fondWeight="bold">
+  <Text>{username}</Text>
+  </CardHeader>
+  <CardBody fontSize="md" fondWeight="bold">
+  <Text>designation:{designation}</Text>
+  </CardBody>
+  <CardFooter fontSize="md" fondWeight="bold">
 
-        <span>Designation : {designation}</span>
+  <Text>department:{department}</Text>
+</CardFooter>
+</Card>
+<SimpleGrid columns={1} spacing={2} marginTop="2rem">
+  <Card borderBottomWidth="10px"
+            align="flex-start"
+            borderRadius="lg"
+            textAlign="left"
+            direction={{ base: "column", sm: "row" }}
+            overflow="hidden"
+            variant="outline"
+            position="sticky"
+            top="1"
+            zIndex="1"
+            padding="0.5em"
+            boxShadow="lg"
+            backgroundColor="gray.100"
+            >
+    <CardHeader fontSize="md" fondWeight="bold">
+      <Text>Issue Id</Text>
+    </CardHeader>
+   <CardBody fontSize="md" fondWeight="bold">
+      <Text>Issue Description</Text>
+    </CardBody>
+    <CardBody fontSize="md" fondWeight="bold">
+      <Text> Item Make</Text>
 
-        <span>Department : {department}</span>
-      </div>
-      <table className="table table-bordered">
-        <thead>
-          <tr>
-            <th>Issue ID</th>
-            <th>Item Description</th>
-            <th>Item Make</th>
-            <th>Item Category</th>
-            <th>Item Valuation</th>
-          </tr>
-        </thead>
+    </CardBody>
+    <CardBody fontSize="md" fondWeight="bold">
+      <Text> Item Category</Text>
+      </CardBody>
+      <CardFooter fontSize="md" fondWeight="bold">
+        <Text> Item Valuation</Text>
+        </CardFooter>
+            </Card>
+            {
+            issues.map((issue) => (
+              <Card 
+            align="flex-start"
+            borderRadius="lg"
+            textAlign="left"  
+            direction={{ base: "column", sm: "row" }}
+            overflow="hidden"
+            >
+                <CardHeader fontSize="md" fondWeight="bold">
+                  <Text>{issue.issueId}</Text>
+                </CardHeader>
+                <CardBody fontSize="md" fondWeight="bold">
+                  <Text>{issue.item.itemDescription}</Text>
+                </CardBody>
+                <CardBody fontSize="md" fondWeight="bold">
+                  <Text>{issue.item.itemMake}</Text>
+                </CardBody>
+                <CardBody fontSize="md" fondWeight="bold">
+                  <Text>{issue.item.itemCategory}</Text>
+                </CardBody>
+                <CardFooter fontSize="md" fondWeight="bold">
+                  <Text>{issue.item.itemValuation}</Text>
+                </CardFooter>
+            </Card>
+            ))
+            }
+          </SimpleGrid>
 
-        <tbody>
-          {issues.map((issue) => (
-            <tr key={issue.issueId}>
-              <td>{issue.issueId}</td>
-              <td>{issue.item.itemDescription}</td>
-              <td>{issue.item.itemMake}</td>
-              <td>{issue.item.itemCategory}</td>
-              <td>{issue.item.itemValuation}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
+
+</div>
   );
 };
 
--- frontend/loanapp/src/components/ViewLoan.jsx
+++ frontend/loanapp/src/components/ViewLoan.jsx
@@ -3,6 +3,11 @@ import { Form, Button, Container } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { getAllEmployeeCards, getEmployee } from "../api/service";
 import { successToast, failureToast } from "../utils/ToastUtils";
+import { Card, CardBody, CardFooter, CardHeader } from "@chakra-ui/react"
+import { Text } from "@chakra-ui/react"
+import { SimpleGrid } from "@chakra-ui/react"
+import { Stack, HStack } from "@chakra-ui/react"
+import { EditIcon, DeleteIcon, CloseIcon, CheckIcon } from "@chakra-ui/icons"
 
 const ViewLoan = ({ loginUser }) => {
 
@@ -46,36 +51,90 @@ const ViewLoan = ({ loginUser }) => {
     };
 
     return (
-        <div style={{ width: '100%' }}>
-            <h1>Loan</h1>
-            <div style={{ width: '100%', display: 'flex', justifyContent: 'space-evenly' }}>
-                    <span> Employee ID : {user}</span>
+        // <div style={{ width: '100%' }}>
+        //     <h1>Loan</h1>
+        //     <div style={{ width: '100%', display: 'flex', justifyContent: 'space-evenly' }}>
+        //             <span> Employee ID : {user}</span>
                 
-                    <span>Designation : {designation}</span>
+        //             <span>Designation : {designation}</span>
                 
-                    <span>Department : {department}</span>
-            </div>
-            <table className="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>Loan ID</th>
-                        <th>Loan Type</th>
-                        <th>Duration of Years</th>
-                        <th>Card Issue Date</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {loanDetails.map((loan) => (
-                        <tr key={loan.loanID}>
-                            <td>{loan.loanCard.loanId}</td>
-                            <td>{loan.loanCard.loanType}</td>
-                            <td>{loan.loanCard.durationOfYears}</td>
-                            <td>{loan.cardIssueDate}</td>
-                        </tr>
-                    ))}
-                </tbody>
+        //             <span>Department : {department}</span>
+        //     </div>
+        //     <table className="table table-bordered">
+        //         <thead>
+        //             <tr>
+        //                 <th>Loan ID</th>
+        //                 <th>Loan Type</th>
+        //                 <th>Duration of Years</th>
+        //                 <th>Card Issue Date</th>
+        //             </tr>
+        //         </thead>
+        //         <tbody>
+        //             {loanDetails.map((loan) => (
+        //                 <tr key={loan.loanID}>
+        //                     <td>{loan.loanCard.loanId}</td>
+        //                     <td>{loan.loanCard.loanType}</td>
+        //                     <td>{loan.loanCard.durationOfYears}</td>
+        //                     <td>{loan.cardIssueDate}</td>
+        //                 </tr>
+        //             ))}
+        //         </tbody>
 
-            </table>
+        //     </table>
+        // </div>
+        <div style={{padding:"2rem"}}>
+        <Card direction={{base:"column",sm:"row"}}
+        variant="outline"
+        position="sticky"
+        top="1"
+        zIndex="1"
+        marginTop="2rem"
+        
+>
+<CardHeader fontSize="md" fondWeight="bold">
+  <Text>{user}</Text>
+  </CardHeader>
+  <CardBody fontSize="md" fondWeight="bold">
+  <Text>designation:{designation}</Text>
+  </CardBody>
+  <CardFooter fontSize="md" fondWeight="bold">
+
+  <Text>department:{department}</Text>
+</CardFooter>
+</Card>
+
+
+<SimpleGrid columns={1} spacing="10px" marginTop="2rem">
+<Card borderBottomWidth="10px" align="flex-start" borderRadius="lg" textAlign="left" direction={{ base: 'column', sm: 'row' }}
+          overflow='hidden'
+          variant='outline'
+          position="sticky"
+          top="1"
+          zIndex="1"
+          padding="0.5em"
+          boxShadow="lg"
+          backgroundColor="gray.100"
+        >
+            <CardHeader fontSize="md" fondWeight="bold"> Loan ID</CardHeader>
+            <CardBody fontSize="md" fondWeight="bold">Loan Type</CardBody>
+            <CardBody fontSize="md" fondWeight="bold">Duration of Years</CardBody>
+            <CardBody fontSize="md" fondWeight="bold">Card Issue Date</CardBody>
+            </Card>
+            {loanDetails.map((loan) => (
+                <Card borderBottomWidth="10px" align="flex-start" borderRadius="lg" textAlign="left" direction={{ base: 'column', sm: 'row' }}
+                overflow='hidden'
+                variant='outline'
+                >
+
+                    <CardHeader fontSize="md" fondWeight="bold">{loan.loanCard.loanId}</CardHeader>
+                    <CardBody fontSize="md" fondWeight="bold">{loan.loanCard.loanType}</CardBody>
+                    <CardBody fontSize="md" fondWeight="bold">{loan.loanCard.durationOfYears}</CardBody>
+                    <CardBody fontSize="md" fondWeight="bold">{loan.cardIssueDate}</CardBody>
+
+                </Card> 
+            ))}
+            </SimpleGrid>
+
         </div>
     );
 };
